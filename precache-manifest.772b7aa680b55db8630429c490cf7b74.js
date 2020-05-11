e264a4b0f743d13900bcd9297d674505fbfc12fc
bondhansarker <bondhansarker100@gmail.com>
2020-05-11 09:20:20 +0600

    Updates

---
+++ precache-manifest.772b7aa680b55db8630429c490cf7b74.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb453186356726aaa59998bc26e635ac",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "ca8220e69d21aa725ade",
+    "url": "/calculator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1c42fb20b04cc291c683",
+    "url": "/calculator/static/js/2.8bbbd52b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/calculator/static/js/2.8bbbd52b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca8220e69d21aa725ade",
+    "url": "/calculator/static/js/main.7dfb25f3.chunk.js"
+  },
+  {
+    "revision": "1b12143a32480a18c898",
+    "url": "/calculator/static/js/runtime-main.12fe3939.js"
+  },
+  {
+    "revision": "eda4598198a00832fb334d5a4e588ead",
+    "url": "/calculator/static/media/01-thumbnail.eda45981.jpg"
+  },
+  {
+    "revision": "e52270b3af9b353d02078b01acd931ae",
+    "url": "/calculator/static/media/02-thumbnail.e52270b3.jpg"
+  },
+  {
+    "revision": "09c41872ba6c96c6efdcab7f202a71a7",
+    "url": "/calculator/static/media/03-thumbnail.09c41872.jpg"
+  },
+  {
+    "revision": "80c8ff5c4a1a0775616a5541086b1d51",
+    "url": "/calculator/static/media/04-thumbnail.80c8ff5c.jpg"
+  },
+  {
+    "revision": "68d8d9495f59fa5109f3b337d784cacc",
+    "url": "/calculator/static/media/05-thumbnail.68d8d949.jpg"
+  },
+  {
+    "revision": "20456fff3658b40a59a73ae9821a273f",
+    "url": "/calculator/static/media/06-thumbnail.20456fff.jpg"
+  },
+  {
+    "revision": "e0b34f6fc62c82231d2016e7e85d7ed0",
+    "url": "/calculator/static/media/1.e0b34f6f.jpg"
+  },
+  {
+    "revision": "a90b0f86f471b7840c8c3b678620cbf3",
+    "url": "/calculator/static/media/2.a90b0f86.jpg"
+  },
+  {
+    "revision": "cc41e47b8fbdb1c81c375b65071e478c",
+    "url": "/calculator/static/media/2.cc41e47b.jpg"
+  },
+  {
+    "revision": "2df43e3546ec0d6666badb2ddbc716ee",
+    "url": "/calculator/static/media/3.2df43e35.jpg"
+  },
+  {
+    "revision": "e2d2a51c2932205b52f4a5ae1943eb97",
+    "url": "/calculator/static/media/header-bg.e2d2a51c.jpg"
+  },
+  {
+    "revision": "cd7c1129e3739717b3bb5120882e3045",
+    "url": "/calculator/static/media/services2.cd7c1129.jpg"
+  }
+]);
